1554e3841fe160e29dad9175cc1d6019f4ae7e0a
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2024-12-16 16:34:34 +0530

    configured settings.py - added templates and static folders

--- sawm/settings.py
+++ sawm/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'sawm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Added Manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
