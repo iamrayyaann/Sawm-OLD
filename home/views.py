ce3a7a005fcd29d5c7e93dd5b4ba1e7b30fc1a21
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2024-12-20 16:15:59 +0530

    Created an arraylist in home.views.py named prayer_calculation_methods, this list stores the methods by which prayer time is calculated.

--- home/views.py
+++ home/views.py
@@ -3,4 +3,32 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
+    prayer_calculation_methods = [
+    "Jafari / Shia Ithna-Ashari",
+    "University of Islamic Sciences, Karachi",
+    "Islamic Society of North America",
+    "Muslim World League",
+    "Umm Al-Qura University, Makkah",
+    "Egyptian General Authority of Survey",
+    None,  # Index 6 is missing
+    "Institute of Geophysics, University of Tehran",
+    "Gulf Region",
+    "Kuwait",
+    "Qatar",
+    None,  # Index 11 is missing
+    "Majlis Ugama Islam Singapura, Singapore",
+    "Union Organization islamic de France",
+    "Diyanet İşleri Başkanlığı, Turkey",
+    "Spiritual Administration of Muslims of Russia",
+    None,
+    "Dubai (experimental)",
+    "Jabatan Kemajuan Islam Malaysia (JAKIM)",
+    "Tunisia",
+    "Algeria",
+    "KEMENAG - Kementerian Agama Republik Indonesia",
+    "Morocco",
+    "Comunidade Islamica de Lisboa",
+    "Ministry of Awqaf, Islamic Affairs and Holy Places, Jordan"
+]
+
     return render(request, 'index.html')
