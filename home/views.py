37201168ccb3b086498e6f4f711e39ad57997d7c
iamrayyaann <syedrayyaann00@gmail.com>
2024-12-21 13:39:28 +0530

    Fixed bug causing empty method_list

--- home/views.py
+++ home/views.py
@@ -2,17 +2,35 @@ from django.shortcuts import render
 import requests
 
 def home(request):
+    try:
+        response = requests.get('https://api.aladhan.com/v1/methods') # Get the prayer methods
+        response_data = response.json()
+        
+        methods_list = []
+        
+        if response.status_code == 200 and response_data.get('data'): # If the request is successful
+            methods = response_data['data']
+            for key, value in methods.items():
+                name = value.get('name', None)
+            methods_list = [(index, method_info['name']) for index, method_info in methods.items() if 'name' in method_info]
+        else:
+            methods_list = [] # Empty list if there is an error
+    except Exception as e:
+        print(f"Error finding prayer methods: {e}")
+        methods_list = [] # Empty list if there is an error
+        
+    
+        
     if request.method == 'POST':
         input_country = request.POST.get('country')
         input_city = request.POST.get('city')
         input_date = request.POST.get('date')
         input_method = request.POST.get('method')
         
-        if input_method == "not selected":
+        if input_method == "N/A":
             context ={
                 'error_type' : 'Invalid input',
                 'error_message' : 'Select a method',
-                
             }
             return render(request, 'error.html', context)
         
@@ -22,21 +40,9 @@ def home(request):
                 'error_message' : 'Fill all the fields',
             }
             return render(request, 'error.html', context)
-    try:
-        response = requests.get('https://api.aladhan.com/v1/methods') # Get the prayer methods
-        response_data = response.json()
-        
-        if response.status_code == 200 and response_data.get('data'): # If the request is successful
-            methods = response_data['data']
-            methods_list = [(index, method_info['name']) for index, method_info in methods.items()]
-        else:
-            methods_list = [] # Empty list if there is an error
-    except Exception as e:
-        print(f"Error finding prayer methods: {e}")
-        methods_list = [] # Empty list if there is an error
         
     context = {
-        'methods': methods_list
+        'methods': methods_list # Array of methods
     }
     
     return render(request, 'index.html', context)
